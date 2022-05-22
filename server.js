18daab51d5e6d54d3a78a3dd9504d3dfd7b88804
Blake Smethers <smethersblake@gmail.com>
2022-05-22 09:42:05 -0500

    moved files around

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+
+
+app.listen(PORT, () =>
+{
+    console.log(`API server now on port ${PORT}`)
+})
