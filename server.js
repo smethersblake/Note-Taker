d726308c6929a8df5a87d5a6b9789ec8aef78cff
Blake Smethers <smethersblake@gmail.com>
2022-05-22 10:24:20 -0500

    added routes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 3001
 const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
+app.use(express.static(__dirname))
+require('./routes/routes')(app)
 
 
 app.listen(PORT, () =>
